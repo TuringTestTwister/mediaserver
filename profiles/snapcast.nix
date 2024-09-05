d7a2c0f4f5e4e6ddcdc03a30eaeff625646808f1
Ellis Rahhal <ellis@rahhal.com>
2024-09-04 18:10:29 -0700

    added latency to local snapcast client

--- profiles/snapcast.nix
+++ profiles/snapcast.nix
@@ -1,5 +1,12 @@
 { lib, pkgs, hostParams, ... }:
 {
+
+  environment.systemPackages = with pkgs; [
+    snapcast
+  ];
+
+  ## Creates server for all clients (speakers) to connect to
+  ## Provides streams from various sources, including pulse audio sink
   services.snapserver = {
     enable = true;
     codec = "flac";
@@ -15,6 +22,7 @@
   # Should not be needed with openFirewall property above also set
   networking.firewall.allowedTCPPorts = [ 1704 1705 1780 ];
 
+  ## Use local audio (locally connected speaker)
   systemd.services.snapclient = {
     wantedBy = [
       "pulseaudio.service"
@@ -27,7 +35,14 @@
       snapcast
     ];
     script = ''
-      ${pkgs.snapcast}/bin/snapclient -h ::1
+      ${pkgs.snapcast}/bin/snapclient --latency 40 -h ::1
+
+      # ${pkgs.snapcast}/bin/snapclient --player alsa --latency 40 -h ::1
+
+      ## Use pulse instead of alsa
+      ## Requires "pactl move-sink-input <sink-input number> 0" after running
+      ## Otherwise gets in a feedback loop
+      # ${pkgs.snapcast}/bin/snapclient --player pulse -h ::1 --latency 60
     '';
     serviceConfig = {
       ## Needed to get access to pulseaudio
@@ -36,6 +51,7 @@
   };
 
 
+  ## Creates sink for various inputs (spotify, bluetooth)
   systemd.services.snapcast-sink = {
     wantedBy = [
       "pulseaudio.service"
